c2a9f7df9d37c0a609ea0ae1cc9dd1b8a90bd0f0
Josh Armstrong <theartistformerlyknownasjosh@gmail.com>
2012-11-22 11:16:58 -0800

    Creating and saving forms. A bug showed up in persisting collections, might need to change to public arraylists or put in an order attribute

--- src/automaticformannotator/AutomaticFormAnnotatorServlet.java
+++ src/automaticformannotator/AutomaticFormAnnotatorServlet.java
@@ -1,6 +1,9 @@
 package automaticformannotator;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.jdo.PersistenceManager;
 import javax.servlet.http.*;
 
 import org.jsoup.Jsoup;
@@ -8,6 +11,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.google.appengine.api.datastore.KeyFactory;
+
+import automaticformannotator.data.PMF;
 import automaticformannotator.form.Attribute;
 import automaticformannotator.form.Field;
 import automaticformannotator.form.Form;
@@ -25,26 +31,27 @@ public class AutomaticFormAnnotatorServlet extends HttpServlet {
 
 		try {
 			doc = Jsoup.connect(page).get();
-			Elements forms = doc.select("form[action~=.+][id~=.+]");
-			resp.getWriter().println(forms.size() + " forms with action and id attributes");
-
-			for (Element element : forms) {
-				resp.getWriter().println("Form with id '" + element.id() + "'");
-
+			Elements formElements = doc.select("form[action~=.+][id~=.+]");
+			resp.getWriter().println(formElements.size() + " forms with action and id attributes");
+			
+			ArrayList<Form> forms = new ArrayList<Form>();
+			
+			for (Element formElement : formElements) {
+				resp.getWriter().println("Form with id '" + formElement.id() + "'");
 				
 				Attribute idAttr = new Attribute();
 				idAttr.setName("id");
-				idAttr.setValue(element.id());
+				idAttr.setValue(formElement.id());
 				
 				Attribute actionAttr = new Attribute();
 				actionAttr.setName("action");
-				actionAttr.setValue(element.attr("action"));
+				actionAttr.setValue(formElement.attr("action"));
 				
 				Form form = new Form();
 				form.addAttributes(idAttr);
 				form.addAttributes(actionAttr);
 				
-				Elements inputs = element.select("input[type~=.+][name~=.+]:not([type=submit]):not([type=hidden])");
+				Elements inputs = formElement.select("input[type~=.+][name~=.+]:not([type=submit]):not([type=hidden])");
 				resp.getWriter().println(inputs.size() + " inputs in this form:");
 				
 				for (Element input : inputs)
@@ -62,11 +69,27 @@ public class AutomaticFormAnnotatorServlet extends HttpServlet {
 					form.addFields(inputField);
 				}
 				
+				forms.add(form);
+				
 				resp.getWriter().println("");
 			}
 			
 			// At this point 'form' can be saved to disk.
 			resp.getWriter().println("Successfully parsed web page");
+			
+			PersistenceManager pm = PMF.get().getPersistenceManager();
+
+	        try 
+	        {
+	        	for (Form form : forms)
+	        	{
+	        		pm.makePersistent(form);
+	        	}
+	        } 
+	        finally 
+	        {
+	            pm.close();
+	        }
 		}
 		catch (IOException e) 
 		{
---
+++ src/automaticformannotator/data/PMF.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package automaticformannotator.data;
+
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
+
+/**
+ * @author josh
+ *
+ */
+public final class PMF {
+    private static final PersistenceManagerFactory pmfInstance =
+        JDOHelper.getPersistenceManagerFactory("transactions-optional");
+
+    private PMF() {}
+
+    public static PersistenceManagerFactory get() {
+        return pmfInstance;
+    }
+}
