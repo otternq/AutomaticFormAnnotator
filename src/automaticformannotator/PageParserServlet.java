f7195b1c876eed47c331b0999b9fe63891931494
Josh Armstrong <theartistformerlyknownasjosh@gmail.com>
2012-11-22 13:06:01 -0800

    Create page parser servlet, which produces no output besides log messages. index.html should be changed to a jsp that displays at least how many forms are in the database.

--- src/automaticformannotator/PageParserServlet.java
+++ src/automaticformannotator/PageParserServlet.java
@@ -2,8 +2,11 @@ package automaticformannotator;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 import javax.jdo.PersistenceManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 import org.jsoup.Jsoup;
@@ -11,34 +14,40 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import com.google.appengine.api.datastore.KeyFactory;
-
 import automaticformannotator.data.PMF;
 import automaticformannotator.form.Attribute;
 import automaticformannotator.form.Field;
 import automaticformannotator.form.Form;
 
+/**
+ * This servlet processes a web page passed as a parameter to it. It looks
+ * for forms and stores what it finds in the datastore. 
+ * 
+ * @author Josh Armstrong
+ *
+ */
 @SuppressWarnings("serial")
-public class AutomaticFormAnnotatorServlet extends HttpServlet {
+public class PageParserServlet extends HttpServlet {
+	private static final Logger log = Logger.getLogger(PageParserServlet.class.getName());
+	
+	/**
+	 * 
+	 */
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		
-		Document doc = null;
 		String page = req.getParameter("page");
 		
-		resp.getWriter().println("Trying to retrieve '" + page + "'");
+		log.info("Trying to parse '" + page + "'");
 
 		try {
-			doc = Jsoup.connect(page).get();
+			// Have Jsoup try to retrieve and parse the page
+			Document doc = Jsoup.connect(page).get();
+			// Select all the form elements that have an action and id defined
 			Elements formElements = doc.select("form[action~=.+][id~=.+]");
-			resp.getWriter().println(formElements.size() + " forms with action and id attributes");
-			
+			// forms is the list of forms that we create
 			ArrayList<Form> forms = new ArrayList<Form>();
 			
 			for (Element formElement : formElements) {
-				resp.getWriter().println("Form with id '" + formElement.id() + "'");
-				
 				Attribute idAttr = new Attribute();
 				idAttr.setName("id");
 				idAttr.setValue(formElement.id());
@@ -51,13 +60,10 @@ public class AutomaticFormAnnotatorServlet extends HttpServlet {
 				form.addAttributes(idAttr);
 				form.addAttributes(actionAttr);
 				
+				// Pull out all the relevant inputs. This select might need to be changed
 				Elements inputs = formElement.select("input[type~=.+][name~=.+]:not([type=submit]):not([type=hidden])");
-				resp.getWriter().println(inputs.size() + " inputs in this form:");
 				
-				for (Element input : inputs)
-				{
-					resp.getWriter().println("Input: type '" + input.attr("type") + "', name '" + input.attr("name") + "'");
-					
+				for (Element input : inputs) {
 					Attribute nameAttr = new Attribute();
 					nameAttr.setName("name");
 					nameAttr.setValue(input.attr("name"));
@@ -70,34 +76,40 @@ public class AutomaticFormAnnotatorServlet extends HttpServlet {
 				}
 				
 				forms.add(form);
-				
-				resp.getWriter().println("");
 			}
 			
 			// At this point 'form' can be saved to disk.
-			resp.getWriter().println("Successfully parsed web page");
-			
+			log.info("Successfully parsed the form. Now saving to datastore.");
+			// Get the persistance manager from the static persistance manager factory
 			PersistenceManager pm = PMF.get().getPersistenceManager();
 
-	        try 
-	        {
-	        	for (Form form : forms)
-	        	{
+	        try {
+	        	for (Form form : forms) {
 	        		pm.makePersistent(form);
 	        	}
 	        } 
-	        finally 
-	        {
+	        catch (Exception e) {
+	        	log.warning("Could not save form to datastore. " + e.getMessage());
+	        }
+	        finally {
 	            pm.close();
 	        }
 		}
-		catch (IOException e) 
-		{
-			resp.getWriter().println("Could not connect to " + page);
+		catch (IOException e) {
+			log.warning("Could not connect to " + page);
 		}
-		catch (IllegalArgumentException e)
-		{
-			resp.getWriter().println("JSoup could not parse the string '" + page + "'");
+		catch (IllegalArgumentException e)  {
+			log.warning("JSoup could not parse the string '" + page + "'");
+		}
+		
+		String destination = "/";
+		 
+		RequestDispatcher rd = getServletContext().getRequestDispatcher(destination);
+		try {
+			rd.forward(req, resp);
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
